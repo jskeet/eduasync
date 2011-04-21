cd0d5c1f8d1197ae2d2b42c9d51253464e13822a
Jon <Jon@Matte>
2011-04-21 21:46:01 +0100

    Initial commit: the compiler support for method builders, with no real implementation.

---
+++ NonGenericMethodWithoutAwaits/AsyncTaskMethodBuilder.cs
@@ -0,0 +1,40 @@
+﻿#region Copyright and license information
+// Copyright 2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+    public struct AsyncTaskMethodBuilder
+    {
+        public static AsyncTaskMethodBuilder Create()
+        {
+            return new AsyncTaskMethodBuilder();
+        }
+
+        public void SetException(Exception e)
+        {
+            Console.WriteLine("SetException called");
+        }
+
+        public void SetResult()
+        {
+            Console.WriteLine("SetResult called");
+        }
+
+        public Task Task { get { return null; } }
+    }
+}
