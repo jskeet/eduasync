2c3ec57047a3ae606f302c460d024bc77f3aab15
Jon Skeet <skeet@pobox.com>
2011-04-22 08:16:00 +0100

    Added FirstTaskCompletionSource.

---
+++ FirstTaskCompletionSource/AsyncTaskMethodBuilder.cs
@@ -0,0 +1,49 @@
+﻿#region Copyright and license information
+// Copyright 2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+    public struct AsyncTaskMethodBuilder<T>
+    {
+        private readonly TaskCompletionSource<T> source;
+
+        private AsyncTaskMethodBuilder(TaskCompletionSource<T> source)
+        {
+            this.source = source;
+        }
+
+        public static AsyncTaskMethodBuilder<T> Create()
+        {
+            return new AsyncTaskMethodBuilder<T>(new TaskCompletionSource<T>());
+        }
+
+        public void SetException(Exception e)
+        {
+            Console.WriteLine("SetException called");
+            source.SetException(e);
+        }
+
+        public void SetResult(T result)
+        {
+            Console.WriteLine("SetResult called");
+            source.SetResult(result);
+        }
+
+        public Task<T> Task { get { return source.Task; } }
+    }
+}
---
+++ FirstTaskCompletionSource/Program.cs
@@ -0,0 +1,39 @@
+﻿#region Copyright and license information
+// Copyright 2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Threading.Tasks;
+using System;
+
+namespace Eduasync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = Return10Async();
+            Console.WriteLine(task.Result);
+        }
+
+        // Warning CS1998 is about a method with no awaits in... exactly what we're trying to
+        // achieve!
+#pragma warning disable 1998
+        static async Task<int> Return10Async()
+        {
+            return 10;
+        }
+#pragma warning restore 1998
+    }
+}
