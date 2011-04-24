771ee0f52566be8a319f3dfce01f8401a25e718b
Jon Skeet <skeet@pobox.com>
2011-04-24 20:06:07 +0100

    Project to demonstrate preserving all exceptions.

---
+++ ExceptionRewrapping/Program.cs
@@ -0,0 +1,54 @@
+﻿#region Copyright and license information
+// Copyright 2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Threading.Tasks;
+using System;
+using System.IO;
+
+namespace Eduasync
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task<int> task = AwaitMultipleFailures();
+            Console.WriteLine("Number of errors: {0}", task.Result);
+        }
+
+        private static async Task<int> AwaitMultipleFailures()
+        {
+            try
+            {
+                await CauseMultipleFailures().WithAllExceptions();
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("Caught arbitrary exception: {0}", e);
+                return e.InnerExceptions.Count;
+            }
+            // Nothing went wrong, remarkably!
+            return 0;
+        }
+
+        private static Task<int> CauseMultipleFailures()
+        {
+            Exception[] exceptions = { new IOException(), new ArgumentException() };
+            TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
+            tcs.SetException(exceptions);
+            return tcs.Task;
+        }
+    }
+}
---
+++ ExceptionRewrapping/TaskExtensions.cs
@@ -0,0 +1,58 @@
+﻿#region Copyright and license information
+// Copyright 2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Threading.Tasks;
+using System;
+
+namespace Eduasync
+{
+    public static class TaskExtensions
+    {
+        public static TaskAwaiter<T> GetAwaiter<T>(this Task<T> task)
+        {
+            return new TaskAwaiter<T>(task);
+        }
+
+        public static Task<T> WithAllExceptions<T>(this Task<T> task)
+        {
+            TaskCompletionSource<T> tcs = new TaskCompletionSource<T>();
+
+            task.ContinueWith(ignored =>
+            {
+                switch (task.Status)
+                {
+                    case TaskStatus.Canceled:
+                        tcs.SetCanceled();
+                        break;
+                    case TaskStatus.RanToCompletion:
+                        tcs.SetResult(task.Result);
+                        break;
+                    case TaskStatus.Faulted:
+                        // SetException will automatically wrap the original AggregateException
+                        // in another one. The new wrapper will be removed in TaskAwaiter, leaving
+                        // the original intact.
+                        tcs.SetException(task.Exception);
+                        break;
+                    default:
+                        tcs.SetException(new InvalidOperationException("Continuationcalled illegally."));
+                        break;
+                }
+            });
+
+            return tcs.Task;
+        }
+    }
+}
