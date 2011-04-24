c69e15bb8b9f38cb16e9ea98c361ed7aa2407257
Jon Skeet <skeet@pobox.com>
2011-04-24 21:04:46 +0100

    First demonstration of the state machine built by the C# 5 compiler.

---
+++ SimpleStateMachine/Program.cs
@@ -0,0 +1,118 @@
+﻿#region Copyright and license information
+// Copyright 2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Eduasync
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task<int> task = ReturnValueAsyncWithStateMachine();
+            Console.WriteLine(task.Result);
+        }
+
+        private static async Task<int> ReturnValueAsyncWithAssistance()
+        {
+            Task<int> task = Task<int>.Factory.StartNew(() => 5);
+
+            return await task;
+        }
+        
+        private static Task<int> ReturnValueAsyncWithStateMachine()
+        {
+            StateMachine stateMachine = new StateMachine(0);
+            stateMachine.moveNextDelegate = stateMachine.MoveNext;
+            stateMachine.builder = AsyncTaskMethodBuilder<int>.Create();
+            stateMachine.MoveNext();
+            return stateMachine.task;
+        }
+
+        private class StateMachine
+        {
+            // Fields representing local variables
+            private TaskAwaiter<int> awaiter;
+            public Task<int> task;
+
+            // Fields representing awaiters
+
+            // Fields common to all async state machines
+            public AsyncTaskMethodBuilder<int> builder;
+            private int state;
+            public Action moveNextDelegate;
+
+            public StateMachine(int state)
+            {
+                // Pointless: will always be 0. Expect this to be removed from later builds.
+                this.state = state;
+            }
+
+            public void MoveNext()
+            {
+                int result;
+                try
+                {
+                    bool doFinallyBodies = true;
+                    if (state != 1)
+                    {
+                        if (state != -1)
+                        {
+                            task = Task<int>.Factory.StartNew(() => 5);
+                            awaiter = task.GetAwaiter();
+                            if (awaiter.IsCompleted)
+                            {
+                                goto Label_GetResult;
+                            }
+                            state = 1;
+                            doFinallyBodies = false;
+                            awaiter.OnCompleted(moveNextDelegate);
+                        }
+                        return;
+                    }
+                    state = 0;
+                Label_GetResult:
+                    int awaitResult = awaiter.GetResult();
+                    awaiter = new TaskAwaiter<int>();
+                    result = awaitResult;
+                }
+                catch (Exception e)
+                {
+                    state = -1;
+                    builder.SetException(e);
+                    return;
+                }
+                state = -1;
+                builder.SetResult(result);
+            }
+
+
+            // Obsolete: will be removed from later builds.
+            private bool disposing;
+
+            [DebuggerHidden]
+            public void Dispose()
+            {
+                disposing = true;
+                MoveNext();
+                state = -1;
+            }
+        }
+    }
+}
