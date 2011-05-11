e4d07887f72b787901a84a540a8f1f69345c6db5
Jon Skeet <skeet@pobox.com>
2011-05-11 20:27:01 +0100

    General purpose "gosub" (no return values) implemented as coroutines.

---
+++ src/GosubCoroutines/Coordinator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eduasync
+{
+    public sealed class Coordinator
+    {
+        private readonly Stack<Action> actions = new Stack<Action>();
+
+        public Coordinator(Action<Coordinator> targetAction)
+        {
+            actions.Push(() => targetAction(this));
+        }
+
+        public Coordinator(Action targetAction)
+        {
+            actions.Push(targetAction);
+        }
+
+        public void Start()
+        {
+            while (actions.Count > 0)
+            {
+                actions.Pop().Invoke();
+            }
+        }
+
+        public Awaiter Gosub(Action targetAction)
+        {
+            return new Awaiter(this, targetAction);
+        }
+
+        public Awaiter Gosub(Action<Coordinator> targetAction)
+        {
+            return new Awaiter(this, () => targetAction(this));
+        }
+
+        public Awaiter Gosub<T>(Action<T> targetAction, T value)
+        {
+            return new Awaiter(this, () => targetAction(value));
+        }
+
+        public Awaiter Gosub<T>(Action<Coordinator, T> targetAction, T value)
+        {
+            return new Awaiter(this, () => targetAction(this, value));
+        }
+
+        public sealed class Awaiter
+        {
+            private readonly Action targetAction;
+            private readonly Coordinator coordinator;
+
+            public Awaiter GetAwaiter()
+            {
+                return this;
+            }
+
+            internal Awaiter(Coordinator coordinator, Action targetAction)
+            {
+                this.coordinator = coordinator;
+                this.targetAction = targetAction;
+            }
+
+            public bool IsCompleted { get { return false; } }
+
+            public void OnCompleted(Action continuation)
+            {
+                coordinator.actions.Push(continuation);
+                coordinator.actions.Push(targetAction);
+            }
+
+            public void GetResult()
+            {
+            }
+        }
+    }
+}
---
+++ src/GosubCoroutines/Program.cs
@@ -0,0 +1,61 @@
+﻿#region Copyright and license information
+// Copyright 2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+
+namespace Eduasync
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Coordinator coordinator = new Coordinator(EntryPoint);
+            coordinator.Start();            
+        }
+
+        private static async void EntryPoint(Coordinator coordinator)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Gosub HandleEven");
+                    await coordinator.Gosub(HandleEven, i);
+                }
+                else
+                {
+                    Console.WriteLine("Gosub HandleEven");
+                    await coordinator.Gosub(HandleOdd, i);
+                }
+            }
+        }
+
+        private static async void HandleEven(Coordinator coordinator, int value)
+        {
+            await coordinator.Gosub(Print, value + " is even");
+        }
+
+        private static async void HandleOdd(Coordinator coordinator, int value)
+        {
+            await coordinator.Gosub(Print, value + " is odd");
+        }
+
+        private static void Print(string message)
+        {
+            Console.WriteLine("Message: {0}", message);
+        }
+    }
+}
