1e8abe9f018cf0156df1dd64934c351ef6074ba8
Jon <Jon@Reggie>
2011-06-28 07:13:07 +0100

    Extra project - directed coroutines.

---
+++ src/DirectedCoroutines/Coordinator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eduasync
+{
+    internal sealed class Coordinator
+    {
+        private readonly Dictionary<Action<Coordinator>, Action> continuationMap =
+            new Dictionary<Action<Coordinator>, Action>();
+
+        private Action<Coordinator> currentMethod;
+        private Action<Coordinator> nextMethod;
+        private Action nextAction;
+
+        public void Start(Action<Coordinator> method)
+        {
+            YieldTo(method);
+            while (nextAction != null)
+            {
+                currentMethod = nextMethod;
+                nextMethod = null;
+                Action tmp = nextAction;
+                nextAction = null;
+                tmp();
+            }
+        }
+
+        public Coordinator YieldTo(Action<Coordinator> method)
+        {
+            Action action;
+            if (!continuationMap.TryGetValue(method, out action))
+            {
+                action = () => method(this);
+            }
+            nextAction = action;
+            nextMethod = method;
+            return this;
+        }
+
+        public Coordinator GetAwaiter()
+        {
+            return this;
+        }
+
+        public bool IsCompleted { get { return false; } }
+
+        public void OnCompleted(Action continuation)
+        {
+            continuationMap[currentMethod] = continuation;
+        }
+
+        public void GetResult()
+        {
+        }
+    }
+}
---
+++ src/DirectedCoroutines/Program.cs
@@ -0,0 +1,64 @@
+﻿#region Copyright and license information
+// Copyright 2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace Eduasync
+{
+    internal class Program
+    {
+        private static readonly Queue<int> values = new Queue<int>();
+
+        private static void Main(string[] args)
+        {
+            Coordinator coordinator = new Coordinator();
+            coordinator.Start(Producer);
+        }
+
+        private static async void Producer(Coordinator coordinator)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enqueuing {0}", i);
+                values.Enqueue(i);
+                if (values.Count == 3)
+                {
+                    Console.WriteLine("Queue full; switching to consumer");
+                    await coordinator.YieldTo(Consumer);
+                }
+            }
+            // Dequeue any remaining items
+            await coordinator.YieldTo(Consumer);
+        }
+
+        private static async void Consumer(Coordinator coordinator)
+        {
+            int loopCount = 0;
+            while (true)
+            {
+                Console.WriteLine("    Entering consumer loop; count {0}", loopCount);
+                loopCount++;
+                while (values.Count != 0)
+                {
+                    Console.WriteLine("    Dequeued {0}", values.Dequeue());
+                }
+                Console.WriteLine("    Queue empty; switching to producer");
+                await coordinator.YieldTo(Producer);
+            }
+        }
+    }
+}
